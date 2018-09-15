2b0d28b7cb593dc189a02665722bb714f9d50b1f
Danny Noowen <nguyendanny91@gmail.com>
2018-09-14 23:45:29 -0700

    submission

--- calc.js
+++ calc.js
@@ -1,92 +1,104 @@
-// Vu - filter logic to create 3 arrays
-  array 1 - firstNumberArray - Vu
-  array 2 - secondNumberArray - Vu
-  array 3 - Operator - Vu
-  var 4 - errorFlag - boolean
+$(document).ready(function() {
+    // var number = [0,1,2,3,4,5,6,7,8,9]
+    // var operators = ["+","-","*","/","^"]
+    // var equalSign="="
+    var firstNumber=[]
+    var secondNumber =[]
+    var operator=[]
+    var wholeThing = []
+    var operatorHit = false;
+    var operatorLock = false;
 
-// Ashley record function keyp presses
-// on button press - capture button presses
+    $(".btn").on("click", function() {
+        var every = $(this).attr("type")
+        wholeThing.push(every)
+        console.log(wholeThing)
+        var flagerror = false
+        if (wholeThing[wholeThing.length-1] =="operator"){
+            if (wholeThing[wholeThing.length-2]=="operator") {
+                flagerror =true;
+                console.log("Error appearsError appearsError appearsError appears")
+            }
+            operatorHit = true;
+            console.log(wholeThing[wholeThing.length-1])
+            console.log(operatorHit)
+        }
+        var key = $(this).attr("value")
+        if (operatorLock) {
+            secondNumber.push(key)
+        }   else    {
+                if (operatorHit){
+                    operator.push(key)
+                    operatorLock = true;
+                } else{
+                    firstNumber.push(key)
+            }
+        }
+        console.log("first "+ firstNumber)
+        console.log("second "+secondNumber)
+        console.log("oper "+ operator)
+        })
 
-// Dnyn - display first number array, second number, array, Result
+    $(".btn").on("click", function() {
+         // display firstnumber function
+         var firstNumberCombine =[];
+         $.each(firstNumber, function(index, value) {
+             firstNumberCombine.push(value);
+         });
+         $('#first-number').html(firstNumberCombine);
 
-// Chuck - combine arrays to create 'whole numbers'
-  firstNumber
-  secondNumber
-  Result
-
-
-
-// Danny display user InputNumbers
-  // if user hits 2 operators then error
-
-// Ashley will create the array [1,2,3,"+","4"]
-  logic to only allow numbers entered first, followed by operator, followed by number
-
-// Chuck you'll formulate the calculations using  and populate into result variables
-  var 1 - firstnumber
-  var 2 - secondNumber
-  var 3 - operator
-  var 4 - result
-
-
-
-
-
-
-var InputNumbers = [1,2,3,"+","4","="];
-var FormulaArray = [123,+,4]
-var DisplayAnswer = "";
-
-var firstNumber = array
-var secondNumber = array
-var operator = array
-var result
-
-firstNumber, secondNumber, operator, result
-
-
-// recordkeystrokes
-  // only allow users to select numbers in the begining populate in firstnumber
-  // if user selects operator then populate operator
-  // only allow users to select numbers
-
-
-// Ashley create array that will record button presses
-
-// Chuck will create the answer - needs to use Ashley's array
-
-// Display Buttons Selected /* DNYN */
-//123 --
-//+
-//4
-//=
+         // operator
+         $('#operator').text(operator);
 
 
+        // display secondnumber function
+        var secondNumberCombine =[];
+        $.each(secondNumber, function(index, value) {
+        secondNumberCombine.push(value);
+        });
+        $('#second-number').html(secondNumberCombine);
+        
 
-//user selects button only numbers allowed on the top starpower
-// when user enters an operator then new row is created and operator is populated
-// when user enters next set of numbers then repeat
+        //
+        //   // operator
+        //   $('#result').text(result);
+        })
+    //
+    // var firstNumber = [4,5,6];
+    // var firstNumberCombine =[]
+    //
+    // var secondNumber = [5,6,7];
+    // var secondNumberCombine = [];
+    //
+    // var operator = "*";
+    // var result = 98230982;
+    //
+    // var errorFlag = false;
+    //
+    //
+    // if (flagerror == false) {
+    //
+    //    // display firstnumber function
+    //    $.each(firstNumber, function(index, value) {
+    //        firstNumberCombine.push(value);
+    //    });
+    //    $('#first-number').html(firstNumberCombine);
+    //
+    //
+    //    // secondnumber function
+    //    $.each(secondNumber, function(index, value) {
+    //        secondNumberCombine.push(value);
+    //    });
+    //    $('#second-number').html(secondNumberCombine);
+    //
+    //    // operator
+    //    $('#operator').text(operator);
+    //
+    //    // result
+    //    $('#result').text(result);
+    //    }
+    // else {
+    //    $('#first-number').text("Error");
+    //    };
 
-
-
-/* array is created to include all buttons in order .. first element is 1 and element is 2
-
-
-
-/* display 1 ,2 ,3 , and last number in third starpower
-
-/* from array transform mathmetic formula
-
-
-
-
-For the next 30 minutes, your goals should be to:
-
-Begin creating sets of variables that you think you will need. As a few suggestions:
-
-firstNumber, secondNumber, operator, result
-Create code that captures the numbers from button clicks, and then displays them on the HTML.
-
-Create code that captures an operator click, then immediately tells your code to begin recording the second number. (Example: If a user clicks the “plus” button, you know they are done typing the first number).
-
-Create code that checks which operator the user has clicked, then saves this operator for later use in a conditional statement. (Example: If a user clicks the “plus” button, you know you will need to be adding two numbers. If a user clicks the “minus” button, you will need to be subtracting two numbers. Think: if-else statements!)
+})
