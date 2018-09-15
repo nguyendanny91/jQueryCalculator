156143001be8de94f56b9c7ef0a73aba84805d05
Danny Noowen <nguyendanny91@gmail.com>
2018-09-14 22:34:37 -0700

    dnyn display

---
+++ calc.js
@@ -0,0 +1,92 @@
+// Vu - filter logic to create 3 arrays
+  array 1 - firstNumberArray - Vu
+  array 2 - secondNumberArray - Vu
+  array 3 - Operator - Vu
+  var 4 - errorFlag - boolean
+
+// Ashley record function keyp presses
+// on button press - capture button presses
+
+// Dnyn - display first number array, second number, array, Result
+
+// Chuck - combine arrays to create 'whole numbers'
+  firstNumber
+  secondNumber
+  Result
+
+
+
+// Danny display user InputNumbers
+  // if user hits 2 operators then error
+
+// Ashley will create the array [1,2,3,"+","4"]
+  logic to only allow numbers entered first, followed by operator, followed by number
+
+// Chuck you'll formulate the calculations using  and populate into result variables
+  var 1 - firstnumber
+  var 2 - secondNumber
+  var 3 - operator
+  var 4 - result
+
+
+
+
+
+
+var InputNumbers = [1,2,3,"+","4","="];
+var FormulaArray = [123,+,4]
+var DisplayAnswer = "";
+
+var firstNumber = array
+var secondNumber = array
+var operator = array
+var result
+
+firstNumber, secondNumber, operator, result
+
+
+// recordkeystrokes
+  // only allow users to select numbers in the begining populate in firstnumber
+  // if user selects operator then populate operator
+  // only allow users to select numbers
+
+
+// Ashley create array that will record button presses
+
+// Chuck will create the answer - needs to use Ashley's array
+
+// Display Buttons Selected /* DNYN */
+//123 --
+//+
+//4
+//=
+
+
+
+//user selects button only numbers allowed on the top starpower
+// when user enters an operator then new row is created and operator is populated
+// when user enters next set of numbers then repeat
+
+
+
+/* array is created to include all buttons in order .. first element is 1 and element is 2
+
+
+
+/* display 1 ,2 ,3 , and last number in third starpower
+
+/* from array transform mathmetic formula
+
+
+
+
+For the next 30 minutes, your goals should be to:
+
+Begin creating sets of variables that you think you will need. As a few suggestions:
+
+firstNumber, secondNumber, operator, result
+Create code that captures the numbers from button clicks, and then displays them on the HTML.
+
+Create code that captures an operator click, then immediately tells your code to begin recording the second number. (Example: If a user clicks the “plus” button, you know they are done typing the first number).
+
+Create code that checks which operator the user has clicked, then saves this operator for later use in a conditional statement. (Example: If a user clicks the “plus” button, you know you will need to be adding two numbers. If a user clicks the “minus” button, you will need to be subtracting two numbers. Think: if-else statements!)
